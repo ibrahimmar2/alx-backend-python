3eec589ba042a78df059f58f1b1b91563beef8f8
ibrahim <irahimammar811@gmail.com>
2024-08-15 20:43:33 +0300

    Task 0-add.py is Done

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+Basic Annotations - add
+Function to write a type-annotated function add
+Takes two float a, b and return their sum as float
+"""
+
+def add(a: float, b: float) -> float:
+    return a + b
